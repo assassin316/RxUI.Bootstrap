2227cf0e1537f900c7ac9b4f0eaf6ba9732d6bcb
Massimo <massimo.cacchiotti@nventive.com>
2018-01-06 13:18:00 -0500

    Added PackageTargetFallback to net standard projects and removed unused code

--- RxUI.UWP/App.xaml.cs
+++ RxUI.UWP/App.xaml.cs
@@ -28,60 +28,6 @@ using Windows.UI.Xaml.Navigation;
 
 namespace RxUI.UWP
 {
-	//	static class DependencyResolverExtensions
-	//	{
-	//		public static void RegisterViewsForViewModels(this IMutableDependencyResolver resolver, Assembly assembly)
-	//		{
-	//			// for each type that implements IViewFor
-	//			foreach (var ti in assembly.DefinedTypes
-	//				.Where(ti => ti.ImplementedInterfaces.Contains(typeof(IViewFor)))
-	//				.Where(ti => !ti.IsAbstract))
-	//			{
-	//				var ivf = ti.ImplementedInterfaces.SingleOrDefault(t => t.GetTypeInfo().ImplementedInterfaces.Contains(typeof(IViewFor)));
-
-	//				// need to check for null because some classes may implement IViewFor but not IViewFor<T> - we don't care about those
-	//				if (ivf != null)
-	//				{
-	//#pragma warning disable CS0436 // Type conflicts with imported type
-	//					var contract = ti.GetCustomAttribute<ViewContractAttribute>()?.Contract;
-	//#pragma warning restore CS0436 // Type conflicts with imported type
-
-	//					RegisterType(resolver, ti, ivf, contract);
-	//				}
-	//			}
-	//		}
-
-	//		public static void Register<TService>(this IMutableDependencyResolver resolver, Func<object> factory, string contract = null)
-	//		{
-	//			resolver.Register(factory, typeof(TService), contract);
-	//		}
-
-	//		public static void RegisterConstant<TService>(this IMutableDependencyResolver resolver, TService value, string contract = null)
-	//		{
-	//			resolver.RegisterConstant(value, typeof(TService), contract);
-	//		}
-
-	//		static void RegisterType(IMutableDependencyResolver resolver, TypeInfo ti, Type serviceType, string contract)
-	//		{
-	//			var factory = TypeFactory(ti.AsType());
-	//			if (ti.GetCustomAttribute<LocatorSingleInstanceAttribute>() != null)
-	//			{
-	//				// *call* factory
-	//				resolver.RegisterConstant(factory(), serviceType, contract);
-	//			}
-	//			else
-	//			{
-	//				// pass in factory
-	//				resolver.Register(factory, serviceType, contract);
-	//			}
-	//		}
-
-	//		static Func<object> TypeFactory(Type type)
-	//		{
-	//			return Expression.Lambda<Func<object>>(Expression.New(type.GetConstructor(Type.EmptyTypes))).Compile();
-	//		}
-	//	}
-
 	public sealed partial class App : BootstrapApplication
 	{
 		public App()
@@ -93,7 +39,6 @@ namespace RxUI.UWP
 			locator.RegisterConstant<ILogger>(new Logger());
 			locator.RegisterConstant<IScreen>(this);
 
-			//locator.RegisterViewsForViewModels(typeof(App).GetTypeInfo().Assembly);
 			Locator.CurrentMutable.RegisterLazySingleton(() => new HomeView(), typeof(IViewFor<HomeViewModel>));
 			Locator.CurrentMutable.RegisterLazySingleton(() => new SecondaryView(), typeof(IViewFor<SecondaryViewModel>));
 		}
--- RxUI.UWP/Bootstrapping/LocatorSingleInstanceAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace RxUI.UWP.Bootstrapping
-{
-    using System;
-
-    [AttributeUsage(AttributeTargets.Class)]
-    public class LocatorSingleInstanceAttribute : Attribute
-    {
-    }
-}
